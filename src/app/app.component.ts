a2fccd69c0b95774075ac7549206315bd4f493e4
Lubomír Ježek <lj@mcrai.eu>
2017-05-04 21:53:19 +0200

    setup app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('open', [
+      state('0', style({ transform: 'translateX(0)' })),
+      state('1',  style({ transform: 'translateX(-320px)' })),
+      transition('* => *', animate('300ms cubic-bezier(.35, 0, .25, 1)'))
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'app works!';
+  @HostBinding('@open') openNavigation = false;
+
+  toggle() {
+    this.openNavigation = !this.openNavigation;
+  }
 }

1b0de12081927c1d61739526df8dc17f72b9f823
Lubomír Ježek <lj@mcrai.eu>
2017-05-08 22:05:08 +0200

    add content into contact page

--- src/app/contact/components/root/root.component.ts
+++ src/app/contact/components/root/root.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-contact',
   templateUrl: './root.component.html',
   styleUrls: ['./root.component.scss']
 })
 export class RootComponent implements OnInit {
+  headline = 'Kontakt';
+  bg = 'url(../../../assets/img/bg-contact.jpg)';
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RootComponent } from './components/root/root.component';
 import { ContactRoutingModule } from './contact-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { UIModule } from '@mcrai/ui';
 
 @NgModule({
   imports: [
     CommonModule,
     ContactRoutingModule,
-    SharedModule
+    SharedModule,
+    UIModule
   ],
   declarations: [
     RootComponent

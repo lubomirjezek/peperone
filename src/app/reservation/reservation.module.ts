c815ee1ca176f9c0b04d589438aa1f977ae82e37
Lubomír Ježek <lj@mcrai.eu>
2017-05-27 19:14:31 +0200

    add inputs to reservation

--- src/app/reservation/reservation.module.ts
+++ src/app/reservation/reservation.module.ts
@@ -4,11 +4,13 @@ import { RootComponent } from './components/root/root.component';
 import { ReservationRoutingModule } from './reservation-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { UIModule } from '@mcrai/ui';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
+    FormsModule,
     ReservationRoutingModule,
     UIModule
   ],

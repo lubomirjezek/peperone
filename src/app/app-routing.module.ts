64a73bfade93958bcdc26ea68c32a4c433673a31
Lubomír Ježek <lj@mcrai.eu>
2017-09-16 17:58:56 +0200

    add title and description to all routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,25 +4,53 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: './homepage/homepage.module#HomepageModule'
+    loadChildren: './homepage/homepage.module#HomepageModule',
+    data: {
+      title: 'Pizzeria a restaurace peperone - Náměšť nad Oslavou',
+      description: 'Typické italské rodinné prostředí s příjemnou atmosférou, milý personál, rychlý servis, čerstvé pokrmy – to vše jsou hodnoty.'
+    }
   }, {
     path: 'denni-menu',
-    loadChildren: './daily-menu/daily-menu.module#DailyMenuModule'
+    loadChildren: './daily-menu/daily-menu.module#DailyMenuModule',
+    data: {
+      title: 'Denní menu a obědy - Náměšť nad Oslavou',
+      description: 'Máte hlad a nevíte kam přes týden na oběd? Pizzeria a restaurace Peperone má každý den skvělé denní menu za lidové ceny.'
+    }
   }, {
     path: 'menu-karta',
-    loadChildren: './menu-card/menu-card.module#MenuCardModule'
+    loadChildren: './menu-card/menu-card.module#MenuCardModule',
+    data: {
+      title: 'Pizza a menu - Náměšť nad Oslavou',
+      description: 'U nás v pizzerii si můžete pochutnat na pizze, která je připravovaná před zraky hostů na šamotovém kameni, osvěžujících salátech, domácích těstovinách, ale i výborných burgerech.'
+    }
   }, {
     path: 'galerie',
-    loadChildren: './gallery/gallery.module#GalleryModule'
+    loadChildren: './gallery/gallery.module#GalleryModule',
+    data: {
+      title: 'Pizzeria a restaurace peperone - Náměšť nad Oslavou',
+      description: 'Typické italské rodinné prostředí s příjemnou atmosférou, milý personál, rychlý servis, čerstvé pokrmy – to vše jsou hodnoty.'
+    }
   }, {
     path: 'kontakt',
-    loadChildren: './contact/contact.module#ContactModule'
+    loadChildren: './contact/contact.module#ContactModule',
+    data: {
+      title: 'Pizzeria a restaurace peperone - Náměšť nad Oslavou',
+      description: 'Typické italské rodinné prostředí s příjemnou atmosférou, milý personál, rychlý servis, čerstvé pokrmy – to vše jsou hodnoty.'
+    }
   }, {
     path: 'rezervace',
-    loadChildren: './reservation/reservation.module#ReservationModule'
+    loadChildren: './reservation/reservation.module#ReservationModule',
+    data: {
+      title: 'Pizzeria a restaurace peperone - Náměšť nad Oslavou',
+      description: 'Typické italské rodinné prostředí s příjemnou atmosférou, milý personál, rychlý servis, čerstvé pokrmy – to vše jsou hodnoty.'
+    }
   }, {
     path: ':category',
-    loadChildren: './category/category.module#CategoryModule'
+    loadChildren: './category/category.module#CategoryModule',
+    data: {
+      title: 'Pizzeria a restaurace peperone - Náměšť nad Oslavou',
+      description: 'Typické italské rodinné prostředí s příjemnou atmosférou, milý personál, rychlý servis, čerstvé pokrmy – to vše jsou hodnoty.'
+    }
   }
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { MessageService } from './shared/services/message.service';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { Meta, Title } from '@angular/platform-browser';
+
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
 
 @Component({
   selector: 'body',
@@ -19,11 +25,37 @@ export class AppComponent implements OnInit {
   state = false;
 
   constructor (
-    private messageService: MessageService
+    private messageService: MessageService,
+    private title: Title,
+    private meta: Meta,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit() {
     this.messageService.anchor = this.messageAnchor;
+
+    this.router.events
+      .filter(event => event instanceof NavigationEnd)
+      .map(() => this.activatedRoute)
+      .map(route => {
+        while (route.firstChild) {
+          route = route.firstChild;
+        }
+
+        return route;
+      })
+      .filter(route => route.outlet === 'primary')
+      .mergeMap(route => route.data)
+      .subscribe(data => {
+        this.title.setTitle(data['title']);
+
+        this.meta.removeTag('name="description"');
+        this.meta.addTag({
+          name: 'description',
+          content: data['description']
+        }, false);
+      });
   }
 
   handleStateChange(state: boolean) {

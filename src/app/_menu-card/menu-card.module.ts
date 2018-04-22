92d822833766c4e76ddbed8c6f23d902a4afe0e4
Lubomír Ježek <lj@mcrai.eu>
2018-04-22 13:09:33 +0200

    change how button for downloading menu card looks, and add second one for czech version

--- src/app/_menu-card/menu-card.module.ts
+++ src/app/_menu-card/menu-card.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UIModule } from '@mcrai/ui/index';
+
 import { SharedModule } from '../shared/shared.module';
 import { MenuCardRoutingModule } from './menu-card-routing.module';
-
 import { RootComponent } from './components/root/root.component';
 import { MenuCardService } from './services/menu-card.service';
 import { MenuCardListComponent } from './components/root/menu-card-list/menu-card-list.component';
@@ -11,7 +12,8 @@ import { MenuCardListComponent } from './components/root/menu-card-list/menu-car
   imports: [
     CommonModule,
     SharedModule,
-    MenuCardRoutingModule
+    MenuCardRoutingModule,
+    UIModule
   ],
   declarations: [
     RootComponent,

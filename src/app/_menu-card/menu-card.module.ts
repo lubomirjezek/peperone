ec836e896883a3e55e7fde751f6be0e76564fe08
Lubomir Jezek <lj@mcrai.eu>
2018-06-10 13:01:32 +0200

    add disclaimer coz of gdpr

--- src/app/_menu-card/menu-card.module.ts
+++ src/app/_menu-card/menu-card.module.ts
@@ -6,12 +6,14 @@ import { MenuCardRoutingModule } from './menu-card-routing.module';
 import { RootComponent } from './components/root/root.component';
 import { MenuCardService } from './services/menu-card.service';
 import { MenuCardListComponent } from './components/menu-card-list/menu-card-list.component';
+import { McRAIButtonModule } from 'ui';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
     MenuCardRoutingModule,
+    McRAIButtonModule
   ],
   declarations: [
     RootComponent,
